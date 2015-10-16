c2aa4e79c0cceab5011388486b0d202425e857bc
shuaibmahmoodrao <shuaibmahmood.rao@gmail.com>
2015-10-16 16:45:43 +0100

    AutoCloseableLock

--- utils/src/main/java/locks/AutoCloseableLock.java
+++ utils/src/main/java/locks/AutoCloseableLock.java
@@ -1,11 +1,37 @@
 package locks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
 /**
- * Created by shuaibrao on 16/10/2015.
+ *  AutoCloseableLock allows you to use a Lock with try with resources feature.
  */
 public class AutoCloseableLock implements AutoCloseable {
-    public void close() throws Exception {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutoCloseableLock.class);
+
+    private final Lock lock;
+
+    public AutoCloseableLock(String id, Lock lock, long timeoutMillis) throws InterruptedException {
+        this.lock = lock;
+
+        if (!lock.tryLock()) {
+            LOGGER.debug("waiting on lock[{}]", id);
+            boolean locked = lock.tryLock(timeoutMillis, TimeUnit.MILLISECONDS);
+
+            if (!locked) {
+                throw new RuntimeException("Could not get lock " + id + " within timeout " + timeoutMillis);
+            }
+        }
+
+        LOGGER.debug("acquired lock[{}]", id);
+    }
+
+    public void close() throws Exception {
+        this.lock.unlock();
     }
 }
 
---
+++ utils/src/test/java/locks/AutoCloseableLockTest.java
@@ -0,0 +1,65 @@
+package locks;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * AutoCloseableLockTest
+ */
+public class AutoCloseableLockTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void test_lock_is_locked_on_construction() throws Exception {
+        ReentrantLock reentrantLock = new ReentrantLock();
+
+        AutoCloseableLock autoCloseableLock = new AutoCloseableLock("test", reentrantLock, 500);
+
+        assertTrue(reentrantLock.isLocked());
+
+    }
+
+    @Test
+    public void test_lock_is_released() throws Exception {
+        ReentrantLock reentrantLock = new ReentrantLock();
+
+        AutoCloseableLock autoCloseableLock = new AutoCloseableLock("test", reentrantLock, 500);
+
+        assertTrue(reentrantLock.isLocked());
+
+        autoCloseableLock.close();
+
+        assertFalse(reentrantLock.isLocked());
+
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void test_throws_runtime_exception() throws Exception {
+        DodgyLock dodgyLock = new DodgyLock();
+
+        try (AutoCloseableLock autoCloseableLock = new AutoCloseableLock("test", dodgyLock, 5)) {
+            assertTrue(dodgyLock.isLocked());
+        }
+    }
+
+    private class DodgyLock extends ReentrantLock {
+        @Override
+        public boolean tryLock() {
+            return false;
+        }
+
+        @Override
+        public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+            return false;
+        }
+    }
+}
