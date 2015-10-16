dd328c47c4f493463047d53df09abca3644c70ab
shuaibmahmoodrao <shuaibmahmood.rao@gmail.com>
2015-10-16 16:13:44 +0100

    Initial commit

---
+++ utils/src/main/java/locks/AutoCloseableLock.java
@@ -0,0 +1,11 @@
+package locks;
+
+/**
+ * Created by shuaibrao on 16/10/2015.
+ */
+public class AutoCloseableLock implements AutoCloseable {
+    public void close() throws Exception {
+
+    }
+}
+
