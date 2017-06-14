369e120473998449db2eb9dd11f3a9be2cc2c634
shuaibmahmoodrao <shuaibmahmood.rao@gmail.com>
2017-06-14 23:54:29 +0100

    Scala

--- swing/src/test/java/com/rao/playground/swing/transitions/WarpEffectTest.java
+++ swing/src/test/java/com/rao/playground/swing/transitions/WarpEffectTest.java
@@ -1,10 +1,12 @@
 package com.rao.playground.swing.transitions;
 
 import junit.framework.TestCase;
+import org.junit.Ignore;
 
 /**
  * Created by shuaibrao on 19/10/2015.
  */
+@Ignore
 public class WarpEffectTest extends TestCase {
 
 }
